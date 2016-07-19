22867e4b56c998878ef12c42453a63a4d2df320a
Jeff Puckett <jeff@jeffpuckett.com>
2016-07-19 18:13:05 -0500

    use test data provider

--- tests/UnitConverterTest.php
+++ tests/UnitConverterTest.php
@@ -2,9 +2,21 @@
 
 class UnitConverterTest extends PHPUnit_Framework_TestCase {
 
-  public function testCanFormatBytes(){
+  /**
+    * @dataProvider getByteFormats
+    */
+  public function testCanFormatBytes($bytes, $format, $expected){
 
-    $this->assertTrue( Puckett\UnitConverter::formatBytes(123456) );
+    $this->assertSame( $expected, Puckett\UnitConverter::formatBytes($bytes, $format) );
+
+  }
+
+  public function getByteFormats(){
+
+    return [
+      [1048576, null, '1 MiB'],
+      [2097152, null, '2 MiB']
+    ];
 
   }
 
