7adb831a8db9de5887aa4747c940454199f6497e
Jeff Puckett <jeff@jeffpuckett.com>
2016-07-20 16:00:36 -0500

    fix: unit name

--- tests/UnitConverterTest.php
+++ tests/UnitConverterTest.php
@@ -19,8 +19,8 @@ class UnitConverterTest extends PHPUnit_Framework_TestCase {
     return [
       [1, null, '1 B'],
       [1000, null, '1000 B'],
-//       [1000, ['unit' => 'KB'], '1 KB'],
-      [1024, null, '1 KiB'],
+//       [1000, ['unit' => 'KB'], '1 kB'],
+      [1024, null, '1 kiB'],
       [1048576, null, '1 MiB'],
       [2097152, null, '2 MiB'],
       [1073741824, null, '1 GiB'],
