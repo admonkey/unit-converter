1143471ae44f35fb0983864a742920dcb4da9d9b
Jeff Puckett <jeff@jeffpuckett.com>
2016-07-20 16:01:35 -0500

    use static numbers for unit ratio

--- src/UnitConverter.php
+++ src/UnitConverter.php
@@ -5,11 +5,47 @@ class UnitConverter {
   public static function formatBytes($bytes, $format = null) {
 
       // TODO: validate and trigger warning
-      if(!isset($format['precision'])) $format['precision'] = 2;
+      // if(!isset($format['precision'])) $format['precision'] = 2;
 
-      $base = log($bytes, 1048576);
-      $suffixes = array('B', 'kiB', 'MiB', 'GiB', 'TiB', 'PiB', 'EiB', 'ZiB', 'YiB');
-      return round(pow(1048576, $base - floor($base)), $format['precision']) .' '. $suffixes[floor($base)];
+      // static unit ratio map http://stackoverflow.com/a/5501447/4233593
+      if ($bytes >= 1208925819614629174706176)
+      {
+          $bytes = number_format($bytes / 1208925819614629174706176) . ' YiB';
+      }
+      elseif ($bytes >= 1180591620717411303424)
+      {
+          $bytes = number_format($bytes / 1180591620717411303424) . ' ZiB';
+      }
+      elseif ($bytes >= 1152921504606846976)
+      {
+          $bytes = number_format($bytes / 1152921504606846976) . ' EiB';
+      }
+      elseif ($bytes >= 1125899906842624)
+      {
+          $bytes = number_format($bytes / 1125899906842624) . ' PiB';
+      }
+      elseif ($bytes >= 1099511627776)
+      {
+          $bytes = number_format($bytes / 1099511627776) . ' TiB';
+      }
+      elseif ($bytes >= 1073741824)
+      {
+          $bytes = number_format($bytes / 1073741824) . ' GiB';
+      }
+      elseif ($bytes >= 1048576)
+      {
+          $bytes = number_format($bytes / 1048576) . ' MiB';
+      }
+      elseif ($bytes >= 1024)
+      {
+          $bytes = number_format($bytes / 1024) . ' kiB';
+      }
+      else
+      {
+          $bytes = $bytes . ' B';
+      }
+
+      return $bytes;
 
   }
 
